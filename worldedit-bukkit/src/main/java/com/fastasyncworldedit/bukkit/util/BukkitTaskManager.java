99e7138f9e5d368cc2b18debf302dfff3b99ddde
Brendan Callanan <brendan.business@outlook.com>
2025-11-04 19:32:47 +0000

    refactor: BukkitTaskManager

--- worldedit-bukkit/src/main/java/com/fastasyncworldedit/bukkit/util/BukkitTaskManager.java
+++ worldedit-bukkit/src/main/java/com/fastasyncworldedit/bukkit/util/BukkitTaskManager.java
@@ -1,62 +1,82 @@
 package com.fastasyncworldedit.bukkit.util;
 
-import com.fastasyncworldedit.bukkit.util.scheduler.Scheduler;
+import com.fastasyncworldedit.core.util.FoliaUtil;
 import com.fastasyncworldedit.core.util.TaskManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 import javax.annotation.Nonnull;
+import java.util.concurrent.TimeUnit;
 
 public class BukkitTaskManager extends TaskManager {
 
     private final Plugin plugin;
-    private final Scheduler scheduler;
 
-    public BukkitTaskManager(final Plugin plugin, final Scheduler scheduler) {
+    public BukkitTaskManager(final Plugin plugin) {
         this.plugin = plugin;
-        this.scheduler = scheduler;
     }
 
     @Override
     public int repeat(@Nonnull final Runnable runnable, final int interval) {
-        this.scheduler.runTaskTimer(this.plugin, runnable, interval, interval);
-        return 0;
+        if (FoliaUtil.isFoliaServer()) {
+            this.plugin.getServer().getGlobalRegionScheduler().runAtFixedRate(this.plugin, scheduledTask -> runnable.run(), interval, interval);
+            return 0;
+        }
+        return this.plugin.getServer().getScheduler().scheduleSyncRepeatingTask(this.plugin, runnable, interval, interval);
     }
 
     @Override
     public int repeatAsync(@Nonnull final Runnable runnable, final int interval) {
-        this.scheduler.runTaskTimer(this.plugin, runnable, interval, interval);
-        return 0;
+        if (FoliaUtil.isFoliaServer()) {
+            this.plugin.getServer().getAsyncScheduler().runAtFixedRate(this.plugin, scheduledTask -> runnable.run(), interval * 50L, interval * 50L, TimeUnit.MILLISECONDS);
+            return 0;
+        }
+        return this.plugin.getServer().getScheduler().scheduleAsyncRepeatingTask(this.plugin, runnable, interval, interval);
     }
 
     @Override
     public void async(@Nonnull final Runnable runnable) {
-        this.scheduler.runTaskAsynchronously(this.plugin, runnable);
+        if (FoliaUtil.isFoliaServer()) {
+            this.plugin.getServer().getAsyncScheduler().runNow(this.plugin, scheduledTask -> runnable.run());
+            return;
+        }
+        this.plugin.getServer().getScheduler().runTaskAsynchronously(this.plugin, runnable).getTaskId();
     }
 
     @Override
     public void task(@Nonnull final Runnable runnable) {
-        this.scheduler.runTask(this.plugin, runnable);
+        if (FoliaUtil.isFoliaServer()) {
+            this.plugin.getServer().getGlobalRegionScheduler().run(this.plugin, scheduledTask -> runnable.run());
+            return;
+        }
+        this.plugin.getServer().getScheduler().runTask(this.plugin, runnable).getTaskId();
     }
 
     @Override
     public void later(@Nonnull final Runnable runnable, final int delay) {
-        this.scheduler.runTaskLater(this.plugin, runnable, delay);
+        if (FoliaUtil.isFoliaServer()) {
+            this.plugin.getServer().getGlobalRegionScheduler().runDelayed(this.plugin, scheduledTask -> runnable.run(), delay);
+            return;
+        }
+        this.plugin.getServer().getScheduler().runTaskLater(this.plugin, runnable, delay).getTaskId();
     }
 
     @Override
     public void laterAsync(@Nonnull final Runnable runnable, final int delay) {
-        this.scheduler.runTaskLaterAsynchronously(this.plugin, runnable, delay);
+        if (FoliaUtil.isFoliaServer()) {
+            this.plugin.getServer().getAsyncScheduler().runDelayed(this.plugin, scheduledTask -> runnable.run(), delay * 50L, TimeUnit.MILLISECONDS);
+            return;
+        }
+        this.plugin.getServer().getScheduler().runTaskLaterAsynchronously(this.plugin, runnable, delay);
     }
 
     @Override
     public void cancel(final int task) {
         if (task != -1) {
-            try {
-                Bukkit.getScheduler().cancelTask(task);
-            } catch (Exception e) {
-                // Ignore errors on Folia - task cancellation by ID is not supported
+            if (FoliaUtil.isFoliaServer()) {
+                return;
             }
+            Bukkit.getScheduler().cancelTask(task);
         }
     }
 
